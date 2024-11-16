954cd4f92486f7977a510f74195d933bfb83ea74
fatima safana <asksiyam@gmail.com>
2024-11-16 06:56:33 +0000

    solution level 999

--- js1-wk1-eval/level-999.js
+++ js1-wk1-eval/level-999.js
@@ -6,7 +6,10 @@
 // Look up documentation for Math.abs if you've not used it before
 
 console.log(Math.abs);
+//my prediction;error
 console.log(Math.abs());
+//Nan because no value is passed
 console.log(Math.abs(-100));
+//100 thats the absolute value.
 
 // Run the file with Node to check your answer
