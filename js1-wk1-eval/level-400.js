61fd76adedf74f77f2a9e46abc32c9eb13e7b621
fatima safana <asksiyam@gmail.com>
2024-11-16 05:58:53 +0000

    solution level 400

--- js1-wk1-eval/level-400.js
+++ js1-wk1-eval/level-400.js
@@ -5,10 +5,16 @@ Sometimes I sneak into the fridge at night and steal my housemate's cake
 
 const secret = "cake";
 confession = confession.replaceAll(secret,'x'.repeat(secret.length));
+console.log(confession)
 
 
 // a) How many function calls are there in this file?
+// ans) 3
 // b) Explain what the expression confession.replaceAll(secret,'x'.repeat(secret.length)); is doing
+// ans) it will hide the string value for secret and replace it with 'x' and repeats it the length of the secret
 // c) How many inputs do we pass to replaceAll when we call it? How can you tell?
+// ans) 3 they are inclosed in a bracket
 // d) How any inputs do we pass to repeat when we call it?  How can you tell?
+// ans) 1 enclosed in a bracket
 // e) What kind of statement is on line 7?
+//ans) an assignment statement
