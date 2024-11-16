c0a21c284e409e96bc116f64daf4746ae8ec534e
fatima safana <asksiyam@gmail.com>
2024-11-16 06:00:49 +0000

    solution level 500

--- js1-wk1-eval/level-500.js
+++ js1-wk1-eval/level-500.js
@@ -1,7 +1,9 @@
 
 
 const avgDaysInYear = Math.floor(365 / 12);
+console.log(avgDaysInYear)
 
 // Without running any code,
 // Predict and explain what the value of avgDaysInYear will evaluate to
 // You can use documentation to look up what Math.floor does
+// avgDaysInYear will evaluate to 30, Math.floor will ensure the value is a whole number.
