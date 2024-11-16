a5bf337408638e263787648e7e7b0084d52b7580
fatima safana <asksiyam@gmail.com>
2024-11-16 06:28:02 +0000

    solution level 700

--- js1-wk1-eval/level-700.js
+++ js1-wk1-eval/level-700.js
@@ -4,3 +4,5 @@ console.log(total);
 
 // Without running the code, predict and explain what total will evaluate to
 // Use the JS1 Week 1 prep to help you work
+// console log does not return or store any value. it functions to print on the console, 
+//its value will always be undefined.
