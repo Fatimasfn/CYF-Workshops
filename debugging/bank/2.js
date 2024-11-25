287087f60ecfd43b501fd69a3dba749e6ff588dc
fatima safana <asksiyam@gmail.com>
2024-11-25 18:15:45 +0000

    2.js

--- debugging/bank/2.js
+++ debugging/bank/2.js
@@ -6,7 +6,7 @@ Can you fix the bug in this function
 
 function getLargestNum(arr) {
     let largestNum = 0;
-    for (let i = 0; i <= arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
       if (arr[i] > largestNum) {
         largestNum = arr[i];
       }
@@ -15,4 +15,4 @@ function getLargestNum(arr) {
   }
   let arr = [1, 2, 3, 4, 5];
 
-  getLargestNum(arr)
+  console.log(getLargestNum(arr));
