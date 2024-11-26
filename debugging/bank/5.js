4f0f6c332802674fd6c22b027d75354328387cda
fatima safana <asksiyam@gmail.com>
2024-11-26 16:09:47 +0000

    5.js

--- debugging/bank/5.js
+++ debugging/bank/5.js
@@ -1,5 +1,6 @@
 /*
 This code contains more than one error. Can you find and fix the errors.
+for (let j = 0; j < list2.length; j++) {
 */
 
 let list1 = [1, 2, 3, 4, 5];
@@ -7,11 +8,9 @@ let list2 = [6, 7, 8, 9, 10];
 
 function mergeLists(list1, list2) {
   let mergedList = [];
-  for (let i = 0; i < list1.length; i++) {
+  for (let i = 0, j = 0 ; i < list1.length && j < list2.length; i++, j++ ) {
     mergedList.push(list1[i]);
-    for (let j = 0; j < list2.length; i++) {
-      mergedList.push(list2[j]);
-    }
+    mergedList.push(list2[j]);
   }
   return mergedList;
 }
